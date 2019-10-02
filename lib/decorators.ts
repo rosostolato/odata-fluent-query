23bb2007199c79177e9127d40044f4ae605b656d
rosostolato <eduardo.rosostolato@gmail.com>
2019-10-02 14:17:40 -0300

    using reflect

--- lib/decorators.ts
+++ lib/decorators.ts
@@ -1,3 +1,12 @@
-export const Property = (target: any, key: string) => {  
-  target.__keys = [...(target.__keys || []), key];
+import "reflect-metadata";
+
+const metakeys = Symbol("keys");
+
+export function Property(target: any, key: string) {
+  const keys: string[] = getPropertyKeys(target);
+  Reflect.metadata(metakeys, [...keys, key])(target);
+}
+
+export function getPropertyKeys(target: any) {
+  return Reflect.getMetadata(metakeys, target) || [];
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { OQuery } from '../lib/oquery';
 import { Property } from '../lib/decorators';
-// import { FilterBuilderComplex, FilterBuilderNumber, FilterBuilderString, FilterBuilderDate, FilterBuilderBoolean } from '../lib/filterbuilder.old';
-// import { mk_query } from '../lib/query';
+
 
 class User {
   @Property id: number;
@@ -16,34 +15,10 @@ interface Permission {
   value: boolean;
 }
 
-// const pfb: FilterBuilderComplex<Permission> = {
-//     id: new FilterBuilderNumber('id'),
-//     module: new FilterBuilderString('module'),
-//     value: new FilterBuilderBoolean('value')
-// }
-
-// const fb: FilterBuilderComplex<User> = {
-//     id: new FilterBuilderNumber('id'),
-//     mail: new FilterBuilderString('mail'),
-//     displayName: new FilterBuilderString('displayName'),
-//     createDate: new FilterBuilderDate('createDate')
-// }
-
-// const query = mk_query<User>(
-//     fb, {}, '', {} as any, {} as any
-// );
-
-// const result = query
-//     .Select('id', 'displayName')
-//     .Filter(x => x.mail.Contains('fugro.com'))
-//     .Filter(x => x.id.BiggerThan(5))
-//     .ToString();
-
-// console.log(result);
-
 const result2 = new OQuery<User>(User)
   .select('id', 'displayName')
-  .filter(f => f.displayName.contains('rosostolato'))
+  .filter(f => f.displayName.contains('rosostolato').and(f.id.biggerThan(5)).not())
+  .filter(f => f.mail.contains('fugro.com'))
   .toString();
 
 console.log(result2);

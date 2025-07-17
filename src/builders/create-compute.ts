e51534dc70d24fd88efbe71cf5ffc0428d25527b
Alex Johnson <johnsonav1992@gmail.com>
2025-07-17 04:59:34 -0500

    refactor(query-compute): enhance type definitions and add documentation for compute expressions

--- src/builders/create-compute.ts
+++ src/builders/create-compute.ts
@@ -2,35 +2,9 @@ import { QueryDescriptor } from '../models'
 import { ComputeBuilder, ComputeExpression, ComputeNumber, ComputeString } from '../models/query-compute'
 import { createQuery } from './create-query'
 
-export function createCompute<T>(descriptor: QueryDescriptor) {
-  return (
-    exp: (builder: ComputeBuilder<T>) => ComputeExpression
-  ) => {
-    const builder = createComputeBuilder<T>()
-    const expression = exp(builder)
-    
-    const newDescriptor = {
-      ...descriptor,
-      compute: [...descriptor.compute, expression.toString()]
-    }
-    
-    return createQuery(newDescriptor)
-  }
-}
-
-function createComputeBuilder<T>(): ComputeBuilder<T> {
-  return new Proxy({} as ComputeBuilder<T>, {
-    get(_target, prop) {
-      if (typeof prop === 'symbol') return undefined
-      
-      return getComputeProperty(prop.toString())
-    }
-  })
-}
-
-function getComputeProperty(propertyPath: string): unknown {
+function getComputeExpression(propertyPath: string): unknown {
   return {
-    as: <K extends string>(alias: K) => ({
+    as: <TAlias extends string>(alias: TAlias) => ({
       toString: () => `${propertyPath} as ${alias}`,
       _alias: alias,
       _type: undefined
@@ -39,9 +13,9 @@ function getComputeProperty(propertyPath: string): unknown {
     substring: (start: number, length?: number) => {
       const args = length !== undefined ? `${start},${length}` : start.toString()
 
-      return getComputeProperty(`substring(${propertyPath},${args})`)
+      return getComputeExpression(`substring(${propertyPath},${args})`)
     },
-    length: () => getComputeProperty(`length(${propertyPath})`),
+    length: () => getComputeExpression(`length(${propertyPath})`),
     concat: (...values: (string | ComputeString | ComputeExpression)[]) => {
       const args = values.map(v => {
         if (typeof v === 'string') return `'${v}'`
@@ -55,7 +29,7 @@ function getComputeProperty(propertyPath: string): unknown {
       // beyond just two. This will allow a user to pass many arguments
       // to `.concat` and the nesting will be done behind the scenes.
       if (args.length === 1) {
-        return getComputeProperty(`concat(${propertyPath},${args[0]})`)
+        return getComputeExpression(`concat(${propertyPath},${args[0]})`)
       } else {
         let result = `concat(${propertyPath},${args[0]})`
 
@@ -63,28 +37,54 @@ function getComputeProperty(propertyPath: string): unknown {
           result = `concat(${result},${args[i]})`
         }
 
-        return getComputeProperty(result)
+        return getComputeExpression(result)
       }
     },
     multiply: (value: number | ComputeNumber | ComputeExpression) => 
-      getComputeProperty(`${propertyPath} mul ${value.toString()}`)
+      getComputeExpression(`${propertyPath} mul ${value.toString()}`)
     ,
     divide: (value: number | ComputeNumber | ComputeExpression) => 
-      getComputeProperty(`${propertyPath} div ${value.toString()}`)
+      getComputeExpression(`${propertyPath} div ${value.toString()}`)
     ,
     add: (value: number | ComputeNumber | ComputeExpression) => 
-      getComputeProperty(`${propertyPath} add ${value.toString()}`)
+      getComputeExpression(`${propertyPath} add ${value.toString()}`)
     ,
     subtract: (value: number | ComputeNumber | ComputeExpression) => 
-      getComputeProperty(`${propertyPath} sub ${value.toString()}`),
-    year: () => getComputeProperty(`year(${propertyPath})`),
-    month: () => getComputeProperty(`month(${propertyPath})`),
-    day: () => getComputeProperty(`day(${propertyPath})`),
-    hour: () => getComputeProperty(`hour(${propertyPath})`),
-    minute: () => getComputeProperty(`minute(${propertyPath})`),
-    second: () => getComputeProperty(`second(${propertyPath})`),
-    date: () => getComputeProperty(`date(${propertyPath})`),
-    time: () => getComputeProperty(`time(${propertyPath})`),
+      getComputeExpression(`${propertyPath} sub ${value.toString()}`),
+    year: () => getComputeExpression(`year(${propertyPath})`),
+    month: () => getComputeExpression(`month(${propertyPath})`),
+    day: () => getComputeExpression(`day(${propertyPath})`),
+    hour: () => getComputeExpression(`hour(${propertyPath})`),
+    minute: () => getComputeExpression(`minute(${propertyPath})`),
+    second: () => getComputeExpression(`second(${propertyPath})`),
+    date: () => getComputeExpression(`date(${propertyPath})`),
+    time: () => getComputeExpression(`time(${propertyPath})`),
+  }
+}
+
+function createComputeBuilder<T>(): ComputeBuilder<T> {
+  return new Proxy({} as ComputeBuilder<T>, {
+    get(_target, prop) {
+      if (typeof prop === 'symbol') return undefined
+      
+      return getComputeExpression(prop.toString())
+    }
+  })
+}
+
+export function createCompute<T>(descriptor: QueryDescriptor) {
+  return (
+    exp: (builder: ComputeBuilder<T>) => ComputeExpression
+  ) => {
+    const builder = createComputeBuilder<T>()
+    const expression = exp(builder)
+    
+    const newDescriptor = {
+      ...descriptor,
+      compute: [...descriptor.compute, expression.toString()]
+    }
+    
+    return createQuery(newDescriptor)
   }
 }
 
--- src/models/query-compute.ts
+++ src/models/query-compute.ts
@@ -1,12 +1,19 @@
 export interface ComputeExpression {
   toString(): string
-  as<K extends string>(alias: K): ComputeExpressionWithAlias<K, this>
+  /**
+   * Creates an alias for the computed expression that can be used in subsequent operations like select, filter, or orderby
+   * @param alias The name to assign to the computed value
+   * @example
+   * .compute(c => c.price.multiply(c.quantity).as('totalValue'))
+   * .select('name', 'totalValue')
+   */
+  as<TAlias extends string>(alias: TAlias): ComputeExpressionWithAlias<TAlias, this>
 }
 
-export interface ComputeExpressionWithAlias<K extends string, V> {
+export interface ComputeExpressionWithAlias<TAliasKey extends string, TAliasValue> {
   toString(): string
-  readonly _alias: K
-  readonly _type: V
+  readonly _alias: TAliasKey
+  readonly _type: TAliasValue
 }
 
 export type InferComputeType<T> = T extends ComputeNumber
@@ -18,29 +25,122 @@ export type InferComputeType<T> = T extends ComputeNumber
   : unknown
 
 export interface ComputeNumber extends ComputeExpression {
+  /**
+   * Multiplies this number by another number or compute expression
+   * @param value The number or compute expression to multiply by
+   * @example c.price.multiply(c.quantity) // price * quantity
+   */
   multiply(value: number | ComputeNumber): ComputeNumber
+  
+  /**
+   * Divides this number by another number or compute expression
+   * @param value The number or compute expression to divide by
+   * @example c.total.divide(c.quantity) // total / quantity
+   */
   divide(value: number | ComputeNumber): ComputeNumber
+  
+  /**
+   * Adds another number or compute expression to this number
+   * @param value The number or compute expression to add
+   * @example c.price.add(100) // price + 100
+   */
   add(value: number | ComputeNumber): ComputeNumber
+  
+  /**
+   * Subtracts another number or compute expression from this number
+   * @param value The number or compute expression to subtract
+   * @example c.price.subtract(c.discount) // price - discount
+   */
   subtract(value: number | ComputeNumber): ComputeNumber
 }
 
 export interface ComputeString extends ComputeExpression {
+  /**
+   * Extracts a substring from this string starting at the specified position
+   * @param start The zero-based starting position
+   * @param length Optional length of the substring. If omitted, extracts to the end
+   * @example c.name.substring(0, 3) // first 3 characters
+   * @example c.name.substring(5) // from position 5 to end
+   */
   substring(start: number, length?: number): ComputeString
+  
+  /**
+   * Gets the length of this string as a number
+   * @example c.name.length() // returns the character count
+   */
   length(): ComputeNumber
+  
+  /**
+   * Concatenates this string with one or more other strings or compute expressions
+   * @param values The values to concatenate (strings, compute expressions, etc.)
+   * @example c.firstName.concat(' ', c.lastName) // "John Doe"
+   * @example c.name.concat(' (', c.email, ')') // "John (john@example.com)"
+   */
   concat(...values: (string | ComputeString | ComputeExpression)[]): ComputeString
 }
 
 
 export interface ComputeDate extends ComputeExpression {
+  /**
+   * Adds a duration to this date
+   * @param duration The duration to add (ISO 8601 duration string or another date expression)
+   * @example c.createdDate.add('P1D') // add 1 day
+   */
   add(duration: string | ComputeDate): ComputeDate
+  
+  /**
+   * Subtracts a duration from this date
+   * @param duration The duration to subtract (ISO 8601 duration string or another date expression)
+   * @example c.createdDate.subtract('P1M') // subtract 1 month
+   */
   subtract(duration: string | ComputeDate): ComputeDate
+  
+  /**
+   * Extracts the year component from this date as a number
+   * @example c.birthDate.year() // returns the year (e.g., 1990)
+   */
   year(): ComputeNumber
+  
+  /**
+   * Extracts the month component from this date as a number (1-12)
+   * @example c.birthDate.month() // returns the month (1=January, 12=December)
+   */
   month(): ComputeNumber
+  
+  /**
+   * Extracts the day component from this date as a number (1-31)
+   * @example c.birthDate.day() // returns the day of the month
+   */
   day(): ComputeNumber
+  
+  /**
+   * Extracts the hour component from this date as a number (0-23)
+   * @example c.lastLogin.hour() // returns the hour in 24-hour format
+   */
   hour(): ComputeNumber
+  
+  /**
+   * Extracts the minute component from this date as a number (0-59)
+   * @example c.lastLogin.minute() // returns the minute
+   */
   minute(): ComputeNumber
+  
+  /**
+   * Extracts the second component from this date as a number (0-59)
+   * @example c.lastLogin.second() // returns the second
+   */
   second(): ComputeNumber
+  
+  /**
+   * Extracts the date portion (without time) from this datetime
+   * @example c.createdDateTime.date() // returns just the date part
+   */
   date(): ComputeDate
+  
+  /**
+   * Extracts the time portion (without date) from this datetime
+   * @example c.createdDateTime.time() // returns just the time part
+   */
   time(): ComputeDate
 }
 

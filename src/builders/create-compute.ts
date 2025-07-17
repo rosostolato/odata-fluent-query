687888148ee82fe6f9d3b665c3c74b660e5f81dd
Alex Johnson <johnsonav1992@gmail.com>
2025-07-16 21:03:20 -0500

    feat(compute): add comprehensive property methods for compute expressions

--- src/builders/create-compute.ts
+++ src/builders/create-compute.ts
@@ -1,5 +1,5 @@
 import { QueryDescriptor } from '../models'
-import { ComputeBuilder, ComputeExpression, createComputeProperty } from '../models/query-compute'
+import { ComputeBoolean, ComputeBuilder, ComputeExpression, ComputeNumber, ComputeString } from '../models/query-compute'
 import { createQuery } from './create-query'
 
 export function createCompute<T>(descriptor: QueryDescriptor) {
@@ -25,7 +25,78 @@ function createComputeBuilder<T>(): ComputeBuilder<T> {
         return undefined
       }
       
-      return createComputeProperty(prop.toString())
+      return getComputeProperty(prop.toString())
     }
   })
 }
+
+function getComputeProperty(propertyPath: string): unknown {
+  return {
+    as: <K extends string>(alias: K) => ({
+        toString: () => `${propertyPath} as ${alias}`,
+        _alias: alias,
+        _type: undefined
+      }),
+
+    toString: () => propertyPath,
+    
+    substring: (start: number, length?: number) => {
+      const args = length !== undefined ? `${start},${length}` : start.toString()
+
+      return getComputeProperty(`substring(${propertyPath},${args})`)
+    },
+
+    length: () => getComputeProperty(`length(${propertyPath})`),
+
+    concat: (...values: (string | ComputeString | ComputeExpression)[]) => {
+      const args = values.map(v => {
+        if (typeof v === 'string') {
+          return `'${v}'`
+        } else if (v && typeof v.toString === 'function') {
+          return v.toString()
+        } else {
+          return v
+        }
+      })
+      
+      return getComputeProperty(`concat(${propertyPath},${args.join(',')})`)
+    },
+    
+    multiply: (value: number | ComputeNumber | ComputeExpression) => 
+      getComputeProperty(`${propertyPath} mul ${value.toString()}`)
+    ,
+
+    divide: (value: number | ComputeNumber | ComputeExpression) => 
+      getComputeProperty(`${propertyPath} div ${value.toString()}`)
+    ,
+    add: (value: number | ComputeNumber | ComputeExpression) => 
+      getComputeProperty(`${propertyPath} add ${value.toString()}`)
+    ,
+    subtract: (value: number | ComputeNumber | ComputeExpression) => 
+      getComputeProperty(`${propertyPath} sub ${value.toString()}`),
+
+    and: (value: boolean | ComputeBoolean | ComputeExpression) => 
+      getComputeProperty(`${propertyPath} and ${value.toString()}`),
+
+    or: (value: boolean | ComputeBoolean | ComputeExpression) => 
+      getComputeProperty(`${propertyPath} or ${value.toString()}`),
+
+    not: () => getComputeProperty(`not ${propertyPath}`),
+
+    equals: (value: boolean | number | string | ComputeExpression) => 
+      getComputeProperty(`${propertyPath} eq ${value.toString()}`),
+
+    notEquals: (value: boolean | number | string | ComputeExpression) => 
+      getComputeProperty(`${propertyPath} ne ${value.toString()}`),
+
+    year: () => getComputeProperty(`year(${propertyPath})`),
+    month: () => getComputeProperty(`month(${propertyPath})`),
+    day: () => getComputeProperty(`day(${propertyPath})`),
+    hour: () => getComputeProperty(`hour(${propertyPath})`),
+    minute: () => getComputeProperty(`minute(${propertyPath})`),
+    second: () => getComputeProperty(`second(${propertyPath})`),
+    date: () => getComputeProperty(`date(${propertyPath})`),
+    time: () => getComputeProperty(`time(${propertyPath})`),
+  }
+}
+
